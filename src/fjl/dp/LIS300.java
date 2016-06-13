2d8184c7226d959a44697e954b6fabdbe3548477
Fanjinlong <3984592648@qq.com>
2016-06-13 18:21:41 +0800

    lis

--- src/fjl/dp/LIS300.java
+++ src/fjl/dp/LIS300.java
@@ -55,9 +55,68 @@ public class LIS300 {
 		}
 		return start;
 	}
-
+	 
+    public static int longestIncreasingSubsequence(int[] nums) {
+        int []f = new int[nums.length];
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            f[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] <= nums[i]) {
+                    f[i] = f[i] > f[j] + 1 ? f[i] : f[j] + 1;
+                }
+            }
+            if (f[i] > max) {
+                max = f[i];
+            }
+        }
+        return max;
+    }
+    public static int longestIncreasingSubsequence1(int[] nums) {
+        int[] minLast = new int[nums.length + 1];
+        minLast[0] = -1;
+        for (int i = 1; i <= nums.length; i++) {
+            minLast[i] = Integer.MAX_VALUE;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            // find the first number in minLast > nums[i]
+            int index = binarySearch(minLast, nums[i]);
+            minLast[index] = nums[i];
+        }
+        
+        for (int i = nums.length; i >= 1; i--) {
+            if (minLast[i] != Integer.MAX_VALUE) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+    
+    // find the first number > num
+    private static int binarySearch(int[] minLast, int num) {
+        int start = 0, end = minLast.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (minLast[mid] == num) {
+                start = mid;
+            } else if (minLast[mid] < num) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (minLast[start] > num) {
+            return start;
+        }
+        return end;
+    }
 	public static void main(String[] args) {
 		int[] nums = { 6, 7, 8, 9, 10, 2, 3, 4, 5 };
 		System.out.println(lengthOfLIS(nums));
+		System.out.println(longestIncreasingSubsequence(nums));
+		System.out.println(longestIncreasingSubsequence1(nums));
 	}
 }
